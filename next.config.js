149f94230100b3375808a143e10c47384f9dd59a
Andrii <iluxor1991@gmail.com>
2022-12-27 18:07:16 +0200

    image path

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
   swcMinify: true,
   images: {
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    loader: 'akamai',
+    path: ''
   },
   experimental: {
     appDir: true,
