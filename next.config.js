0a6e28e8323dc23c2421c84be4907c4a2faf8317
Andrii <iluxor1991@gmail.com>
2022-12-27 18:34:28 +0200

    check path

--- next.config.js
+++ next.config.js
@@ -2,13 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/next-portfolio',
-  assetPrefix: '/next-portfolio',
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     loader: 'akamai',
-    path: '/next-portfolio'
+    path: 'https://lastofpudge.github.io/next-portfolio/'
   },
   experimental: {
     appDir: true,
