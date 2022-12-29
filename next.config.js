53c209dfa5f338b1951e919a26f125812a2af3b4
Andrii <iluxor1991@gmail.com>
2022-12-29 12:18:16 +0200

    refactor: test

--- next.config.js
+++ next.config.js
@@ -1,23 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-let env
-
-if (process.env.APP_ENV === 'production') {
-  env = {
-    images: {
-      loader: 'akamai',
-      path: './next-portfolio/images/',
-      dangerouslyAllowSVG: true,
-      contentSecurityPolicy: 'default-src \'self\'; script-src \'none\'; sandbox;'
-    }
-  }
-} else {
-  env = {}
-}
-
 const nextConfig = {
   reactStrictMode: true,
-  env,
+  images: {
+    loader: 'akamai',
+    path: '/next-portfolio/',
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: 'default-src \'self\'; script-src \'none\'; sandbox;'
+  },
   experimental: {
     appDir: true,
     fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }]
