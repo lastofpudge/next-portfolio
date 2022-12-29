08dc5ddadf76f5cdab60cbefce357982181dbc50
Andrii <iluxor1991@gmail.com>
2022-12-29 12:07:03 +0200

    fix: updated env

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+let env
+
+if (process.env.APP_ENV === 'production') {
+  env = {
+    images: {
+      loader: 'akamai',
+      path: './',
+      dangerouslyAllowSVG: true,
+      contentSecurityPolicy: 'default-src \'self\'; script-src \'none\'; sandbox;'
+    }
+  }
+} else {
+  env = {}
+}
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  basePath: process.env.APP_ENV === 'production' ? process.env.BASE_PATH : '',
-  assetPrefix: process.env.APP_ENV === 'production' ? process.env.BASE_PATH + '/' : '',
-  images: {
-    loader: 'akamai',
-    path: '',
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: 'default-src \'self\'; script-src \'none\'; sandbox;'
-  },
+  env,
   experimental: {
     appDir: true,
     fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }]
